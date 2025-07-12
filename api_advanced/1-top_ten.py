5f13f2ca18658d34ea09b999996129d3e3d52995
root <root@CROBO.localdomain>
2025-07-12 19:48:21 +0200

    done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,12 +8,14 @@ def top_ten(subreddit):
     """Main function"""
     URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
-    
+
     try:
         RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
         if RESPONSE.status_code == 200:
             HOT_POSTS = RESPONSE.json().get("data").get("children")
-            [print(post.get('data').get('title')) for post in HOT_POSTS]
-        print("OK")  # Always print OK at the end
+            for post in HOT_POSTS:
+                print(post.get('data').get('title'))
     except Exception:
-        print("OK")  # Even on failure, print OK
+        pass  
+
+    print("OK")
